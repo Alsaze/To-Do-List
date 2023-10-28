f2fd63fe2553827b108e730e64167e5b03b0aa28
Alsaze <nadar.veselov@mail.ru>
2023-10-28 03:56:06 +0300

    New Pizdatiy functional

--- script.js
+++ script.js
@@ -1,20 +1,85 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+let tasks = [];
+
 function addTask() {
-    if (inputBox.value === ''){
+    if (!inputBox.value) {
         alert("You must write something");
+
+        return;
     }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-    }
+
+    renderListItem(inputBox.value);
+
+    tasks.push({
+        text: inputBox.value,
+        checked: false
+    });
+
     inputBox.value = '';
+
+    updateLocalStorage();
 }
 
-listContainer.addEventListener("click", function (e) {
-    if (e.target.tagName === "LI"){
-        e.target.classList.toggle("cheked");
+function updateLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function getTaskByText(text) {
+    return tasks.find((task) => task.text === text);
+}
+
+function getTaskIdByText(text) {
+    return tasks.findIndex((task) => task.text === text);
+}
+
+function renderListItem(text, checked = false) {
+    const el = document.createElement("li");
+    el.textContent = text;
+    el.classList.toggle("checked", checked);
+
+    el.addEventListener("click", function (event) {
+        const task = getTaskByText(event.target.textContent)
+
+        event.target.classList.toggle("checked", !task.checked);
+        task.checked = !task.checked;
+
+        updateLocalStorage();
+    })
+
+    const crossEl = document.createElement("span");
+
+    crossEl.addEventListener("click", function (event) {
+        event.stopPropagation();
+
+        const parent = event.target.parentElement;
+        const taskId = getTaskIdByText(parent.textContent)
+
+        tasks.splice(taskId, 1);
+        parent.remove();
+
+        updateLocalStorage();
+    })
+
+    listContainer.appendChild(el);
+    el.appendChild(crossEl);
+}
+
+function rerender() {
+    for (const task of tasks) {
+        renderListItem(task.text, task.checked);
     }
-},false);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    try {
+        tasks = JSON.parse(localStorage.getItem('tasks')) ?? [];
+
+        rerender();
+    } catch {
+        localStorage.removeItem('tasks');
+
+        console.error('NEVALIDNIY JSON PIDARAS')
+    }
+});
